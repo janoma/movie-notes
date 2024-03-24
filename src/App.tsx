6dba255cd6f2ac16f0b783e197cc321b24a97be0
Alejandro Mallea <janoma@gmail.com>
2024-03-23 21:37:44 -0700

    add firebase and a coverage action

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
 import { Container, Typography } from "@mui/material";
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
 import { FC } from "react";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAY6z5sRBqCnU3hIF0aCSOM0oUnXshxMmw",
+  appId: "1:474448258360:web:8f6acb6cbeb2a345ee0dcd",
+  authDomain: "janoma-movie-notes.firebaseapp.com",
+  measurementId: "G-SHDJ6B2CKR",
+  messagingSenderId: "474448258360",
+  projectId: "janoma-movie-notes",
+  storageBucket: "janoma-movie-notes.appspot.com",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+console.log(analytics.app.name);
 
 const App: FC = () => {
   return (
